ba99abd2d9972f059d88b7f151c36bb18e99adf7
dfsek <dfsek@protonmail.com>
2022-01-02 01:33:17 -0700

    ad ioutils

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
     testImplementation("junit", "junit", "4.12")
     implementation("com.dfsek.terra:base:6.0.0-BETA+f7d53c9db")
     compileOnly("org.jetbrains:annotations:23.0.0")
+    implementation("commons-io:commons-io:2.11.0")
 
 
     implementation("com.fifesoft:rstaui:3.1.3")
