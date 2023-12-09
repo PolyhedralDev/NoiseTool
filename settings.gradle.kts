64fb402b0b0894da7cdfda95cc4580b69f1bf086
Zoë Gidiere <duplexsys@protonmail.com>
2023-12-09 13:22:45 -0700

    use proper ci cache

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,10 @@
 rootProject.name = "NoiseTool"
 
+// settings.gradle.kts
+val isCiServer = System.getenv().containsKey("CI")
+// Cache build artifacts, so expensive operations do not need to be re-computed
+buildCache {
+    local {
+        isEnabled = !isCiServer
+    }
+}
