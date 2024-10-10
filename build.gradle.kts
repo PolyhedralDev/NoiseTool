4aecb19fc46cb4267b265d5011982768a62c58f7
Zoe Gidiere <duplexsys@protonmail.com>
2024-10-09 19:46:10 -0600

    Update to new Terra Version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     java
-    id("com.github.johnrengelman.shadow").version("8.1.1")
+    id("com.gradleup.shadow").version("8.3.3")
 }
 
 group = "com.dfsek"
-version = "1.1.0"
+version = "1.1.1"
 
 repositories {
     mavenCentral()
@@ -23,31 +23,31 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.10.1")
-    implementation("com.dfsek.terra:base:6.4.1-BETA+3aef97738")
-    compileOnly("org.jetbrains:annotations:24.1.0")
-    implementation("commons-io:commons-io:2.15.1")
-    implementation("com.google.guava:guava:32.1.3-jre")
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.11.2")
+    implementation("com.dfsek.terra:base:6.5.0-BETA+c374c2d5e")
+    compileOnly("org.jetbrains:annotations:26.0.0")
+    implementation("commons-io:commons-io:2.17.0")
+    implementation("com.google.guava:guava:33.3.1-jre")
 
     implementation("com.dfsek.tectonic", "yaml", "4.2.1")
-    implementation("ch.qos.logback:logback-classic:1.4.14")
+    implementation("ch.qos.logback:logback-classic:1.5.8")
 
 
     implementation("com.fifesoft:rstaui:3.3.1")
-    implementation("com.fifesoft:rsyntaxtextarea:3.3.4")
+    implementation("com.fifesoft:rsyntaxtextarea:3.5.1")
     implementation("com.fifesoft:autocomplete:3.3.1")
 
-    implementation("com.formdev:flatlaf:3.2.5")
+    implementation("com.formdev:flatlaf:3.5.1")
 
-    implementation("org.jogamp.jogl:jogl-all:2.4.0")
-    runtimeOnly("org.jogamp.jogl:jogl-all:2.4.0:natives-linux-amd64")
-    runtimeOnly("org.jogamp.jogl:jogl-all:2.4.0:natives-macosx-universal")
-    runtimeOnly("org.jogamp.jogl:jogl-all:2.4.0:natives-windows-amd64")
+    implementation("org.jogamp.jogl:jogl-all:2.5.0")
+    runtimeOnly("org.jogamp.jogl:jogl-all:2.5.0:natives-linux-amd64")
+    runtimeOnly("org.jogamp.jogl:jogl-all:2.5.0:natives-macosx-universal")
+    runtimeOnly("org.jogamp.jogl:jogl-all:2.5.0:natives-windows-amd64")
 
-    implementation("org.jogamp.gluegen:gluegen-rt:2.4.0")
-    runtimeOnly("org.jogamp.gluegen:gluegen-rt:2.4.0:natives-linux-amd64")
-    runtimeOnly("org.jogamp.gluegen:gluegen-rt:2.4.0:natives-macosx-universal")
-    runtimeOnly("org.jogamp.gluegen:gluegen-rt:2.4.0:natives-windows-amd64")
+    implementation("org.jogamp.gluegen:gluegen-rt:2.5.0")
+    runtimeOnly("org.jogamp.gluegen:gluegen-rt:2.5.0:natives-linux-amd64")
+    runtimeOnly("org.jogamp.gluegen:gluegen-rt:2.5.0:natives-macosx-universal")
+    runtimeOnly("org.jogamp.gluegen:gluegen-rt:2.5.0:natives-windows-amd64")
 }
 
 
@@ -62,6 +62,6 @@ tasks.build {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
